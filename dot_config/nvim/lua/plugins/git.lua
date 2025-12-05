5323577d680bb2dcfcbff1ccde548155f34cb07f
dykarohora <7d.yama@gmail.com>
2025-12-05 15:32:57 +0900

    git-graphの設定を更新した

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,54 +1,78 @@
 -- nvim v0.8.0
 return {
-  {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
-  },
-  {
-    'isakbm/gitgraph.nvim',
-    opts = {
-      git_cmd = "git",
-      symbols = {
-        merge_commit = 'M',
-        commit = '*',
-      },
-      format = {
-        timestamp = '%H:%M:%S %d-%m-%Y',
-        fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
-      },
-      hooks = {
-        on_select_commit = function(commit)
-          print('selected commit:', commit.hash)
-        end,
-        on_select_range_commit = function(from, to)
-          print('selected range:', from.hash, to.hash)
-        end,
-      },
-    },
-    keys = {
-      {
-        "<leader>gl",
-        function()
-          require('gitgraph').draw({}, { all = true, max_count = 5000 })
-        end,
-        desc = "GitGraph - Draw",
-      },
-    },
-  },
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
+	{
+		"isakbm/gitgraph.nvim",
+		opts = {
+			git_cmd = "git",
+			symbols = {
+				merge_commit = "",
+				commit = "",
+				merge_commit_end = "",
+				commit_end = "",
+
+				-- Advanced symbols
+				GVER = "",
+				GHOR = "",
+				GCLD = "",
+				GCRD = "╭",
+				GCLU = "",
+				GCRU = "",
+				GLRU = "",
+				GLRD = "",
+				GLUD = "",
+				GRUD = "",
+				GFORKU = "",
+				GFORKD = "",
+				GRUDCD = "",
+				GRUDCU = "",
+				GLUDCD = "",
+				GLUDCU = "",
+				GLRDCL = "",
+				GLRDCR = "",
+				GLRUCL = "",
+				GLRUCR = "",
+			},
+			format = {
+				timestamp = "%H:%M:%S %d-%m-%Y",
+				fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+			},
+			hooks = {
+				on_select_commit = function(commit)
+					print("selected commit:", commit.hash)
+				end,
+				on_select_range_commit = function(from, to)
+					print("selected range:", from.hash, to.hash)
+				end,
+			},
+		},
+		keys = {
+			{
+				"<leader>gl",
+				function()
+					require("gitgraph").draw({}, { all = true, max_count = 5000 })
+				end,
+				desc = "GitGraph - Draw",
+			},
+		},
+	},
 }
