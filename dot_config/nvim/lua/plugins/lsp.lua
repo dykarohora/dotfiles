578538bfcdf030472ae1c5c003f2a9667a493860
dykarohora <7d.yama@gmail.com>
2025-12-07 10:55:58 +0900

    Overlookを導入した

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "saghen/blink.cmp",  -- capabilities のため
-    },
-    config = function()
-      require("lsp").setup()
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"saghen/blink.cmp", -- capabilities のため
+		},
+		config = function()
+			require("lsp").setup()
+		end,
+	},
 	{
 		"nvimdev/lspsaga.nvim",
 		config = function()
@@ -84,6 +84,154 @@ return {
 					timeout_ms = 500,
 					lsp_format = "fallback",
 				},
+			})
+		end,
+	},
+	{
+		"WilliamHsieh/overlook.nvim",
+		opts = {
+			ui = {
+				border = "rounded",
+				z_index_base = 30,
+				row_offset = 0,
+				col_offset = 0,
+				stack_row_offset = 0,
+				stack_col_offset = 0,
+				width_decrement = 2,
+				height_decrement = 1,
+				min_width = 10,
+				min_height = 3,
+				size_ratio = 0.65,
+				keys = {
+					close = "q",
+				},
+			},
+		},
+		event = { "BufReadPre", "BufNewFile" },
+		keys = {
+			{
+				"<leader>pd",
+				function()
+					require("overlook.api").peek_definition()
+				end,
+				desc = "Overlook: Peek definition",
+			},
+			{
+				"<leader>pp",
+				function()
+					require("overlook.api").peek_cursor()
+				end,
+				desc = "Overlook: Peek cursor",
+			},
+			{
+				"<leader>pu",
+				function()
+					require("overlook.api").restore_popup()
+				end,
+				desc = "Overlook: Restore last popup",
+			},
+			{
+				"<leader>pU",
+				function()
+					require("overlook.api").restore_all_popups()
+				end,
+				desc = "Overlook: Restore all popups",
+			},
+			{
+				"<leader>pc",
+				function()
+					require("overlook.api").close_all()
+				end,
+				desc = "Overlook: Close all popups",
+			},
+			{
+				"<leader>pf",
+				function()
+					require("overlook.api").switch_focus()
+				end,
+				desc = "Overlook: Switch focus",
+			},
+			{
+				"<leader>ps",
+				function()
+					require("overlook.api").open_in_split()
+				end,
+				desc = "Overlook: Open popup in split",
+			},
+			{
+				"<leader>pv",
+				function()
+					require("overlook.api").open_in_vsplit()
+				end,
+				desc = "Overlook: Open popup in vsplit",
+			},
+			{
+				"<leader>pt",
+				function()
+					require("overlook.api").open_in_tab()
+				end,
+				desc = "Overlook: Open popup in tab",
+			},
+			{
+				"<leader>po",
+				function()
+					require("overlook.api").open_in_original_window()
+				end,
+				desc = "Overlook: Open popup in current window",
+			},
+		},
+		config = function(_, opts)
+			require("overlook").setup(opts)
+
+			local aug = vim.api.nvim_create_augroup("OverlookPopupTweak", { clear = true })
+
+			local function apply_overlook_window_opts()
+				if not vim.w.is_overlook_popup then
+					return
+				end
+
+				-- 他の autocmd で signcolumn が書き換えられたあとに上書きしたいので schedule する
+				vim.schedule(function()
+					if not vim.api.nvim_win_is_valid(0) or not vim.w.is_overlook_popup then
+						return
+					end
+
+					local wo = vim.wo
+					local bo = vim.bo
+
+					-- ★ 左側の揺れ対策の本丸
+					wo.signcolumn = "no"
+					wo.number = false
+					wo.relativenumber = false
+					wo.foldcolumn = "0"
+					wo.statuscolumn = ""
+
+					-- 表示でガチャつきそうなもの
+					wo.list = false
+					wo.breakindent = false
+					wo.cursorline = false
+
+					-- fold 無効化
+					wo.foldenable = false
+					wo.foldmethod = "manual"
+
+					-- 念のためインデント自動調整系もオフ
+					bo.autoindent = false
+					bo.smartindent = false
+					bo.cindent = false
+				end)
+			end
+
+			-- ウィンドウに入ったとき
+			vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
+				group = aug,
+				callback = apply_overlook_window_opts,
+			})
+
+			-- カーソル移動のたびに誰かが signcolumn を auto に戻してきても潰す
+			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+				group = aug,
+				callback = apply_overlook_window_opts,
 			})
 		end,
 	},
