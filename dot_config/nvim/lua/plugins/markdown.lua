3f1787521f4802fe027e68cee9ea907a7cb0f959
dykarohora <7d.yama@gmail.com>
2025-12-15 10:15:05 +0900

    add markdown preview plugin

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,12 +1,20 @@
 return {
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-    ft = { "markdown" },
-    keys = {
-      { "<Space>sm", ":RenderMarkdown toggle<CR>" },
-    },
-    opts = {},
-  }
-
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		ft = { "markdown" },
+		keys = {
+			{ "<Space>sm", ":RenderMarkdown toggle<CR>" },
+		},
+		opts = {},
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
 }
