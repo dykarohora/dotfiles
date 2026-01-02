f2976a2e6467b0699aced97e7dc0a99455411331
dykarohora <7d.yama@gmail.com>
2026-01-02 13:32:54 +0900

    treesitterの設定を修正した

--- dot_config/nvim/lua/plugins/syntax.lua
+++ dot_config/nvim/lua/plugins/syntax.lua
@@ -2,62 +2,71 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
 		build = ":TSUpdate",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
-		main = "nvim-treesitter.configs",
-		opts = {
-			-- 必要な言語は好きに増やしてOK
-			ensure_installed = {
-				"lua",
-				"vim",
-				"vimdoc",
-				"javascript",
-				"typescript",
-				"tsx",
-				"rust",
-				"json",
-				"html",
-				"css",
-			},
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"bash",
+					"go",
+					"rust",
+					"typescript",
+					"javascript",
+					"tsx",
+					"json",
+					"yaml",
+					"html",
+					"css",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
 
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-
-			indent = {
-				enable = true,
-			},
-
-			-- textobjects の設定ここに書く
-			textobjects = {
-				select = {
+				incremental_selection = {
 					enable = true,
-					lookahead = true, -- `af` / `if` みたいなのを次の対象にジャンプしながら選択する
 					keymaps = {
-						-- 例: 関数・クラス単位の選択
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
+						init_selection = "<CR>",
+						node_incremental = "<CR>",
+						node_decremental = "<BS>",
+						scope_incremental = "<S-CR>",
 					},
 				},
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]m"] = "@function.outer",
-					},
-					goto_previous_start = {
-						["[m"] = "@function.outer",
-					},
-				},
-			},
-		},
-	},
 
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]f"] = "@function.outer",
+							["]c"] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+							["[c"] = "@class.outer",
+						},
+					},
+				},
+			})
+		end,
+	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
