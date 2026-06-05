f130a3b8f2d26532a5ea983d3eba90c2e8055e01
dykarohora <7d.yama@gmail.com>
2026-06-05 21:57:14 +0900

    neovim0.12に移行した

--- dot_config/nvim/lua/plugins/syntax.lua
+++ dot_config/nvim/lua/plugins/syntax.lua
@@ -1,67 +1,64 @@
--- Treesitter
+-- Treesitter (Neovim 0.12 + nvim-treesitter main branch)
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPre", "BufNewFile" },
+		branch = "main",
 		build = ":TSUpdate",
 		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
+			{ "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
 		},
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"lua",
-					"vim",
-					"vimdoc",
-					"query",
-					"bash",
-					"go",
-					"rust",
-					"typescript",
-					"javascript",
-					"tsx",
-					"json",
-					"yaml",
-					"html",
-					"css",
-				},
-				highlight = { enable = true },
-				indent = { enable = true },
+			require("nvim-treesitter").setup()
 
-				incremental_selection = {
+			require("nvim-treesitter").install({
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"bash",
+				"go",
+				"rust",
+				"typescript",
+				"javascript",
+				"tsx",
+				"json",
+				"yaml",
+				"html",
+				"css",
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(ev)
+					local ok = pcall(vim.treesitter.start, ev.buf)
+					if ok then
+						vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
+			})
+
+			require("nvim-treesitter-textobjects").setup({
+				select = {
 					enable = true,
+					lookahead = true,
 					keymaps = {
-						init_selection = "<CR>",
-						node_incremental = "<CR>",
-						node_decremental = "<BS>",
-						scope_incremental = "<S-CR>",
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
 					},
 				},
-
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-							["aa"] = "@parameter.outer",
-							["ia"] = "@parameter.inner",
-						},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]f"] = "@function.outer",
+						["]c"] = "@class.outer",
 					},
-					move = {
-						enable = true,
-						set_jumps = true,
-						goto_next_start = {
-							["]f"] = "@function.outer",
-							["]c"] = "@class.outer",
-						},
-						goto_previous_start = {
-							["[f"] = "@function.outer",
-							["[c"] = "@class.outer",
-						},
+					goto_previous_start = {
+						["[f"] = "@function.outer",
+						["[c"] = "@class.outer",
 					},
 				},
 			})
@@ -69,6 +66,7 @@ return {
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
+		-- branch 指定なし: デフォルトの master ブランチを使用（main ブランチは存在しない）
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		event = { "BufNewFile", "BufReadPre" },
 		opts = {
@@ -79,7 +77,7 @@ return {
 			line_numbers = true,
 			multiline_threshold = 20,
 			trim_scope = "outer",
-			mode = "cursor", -- ←ここはだいすけくんの好み次第でOK
+			mode = "cursor",
 			separator = nil,
 			zindex = 20,
 			on_attach = nil,
